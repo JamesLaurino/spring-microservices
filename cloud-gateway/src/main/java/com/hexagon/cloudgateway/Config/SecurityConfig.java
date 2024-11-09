5218f65be398935d96630e2be980997ed26f4b1e
jamesLaurino <petersthomas90@gmail.com>
2024-11-09 19:20:31 +0100

    Sécurisation des routes avec des filtres en inMemory + ajout lombok

--- cloud-gateway/src/main/java/com/hexagon/cloudgateway/Config/SecurityConfig.java
+++ cloud-gateway/src/main/java/com/hexagon/cloudgateway/Config/SecurityConfig.java
@@ -2,29 +2,56 @@ package com.hexagon.cloudgateway.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    public ReactiveUserDetailsService userDetailsService() {
+        UserDetails userJames = User.builder()
+                .username("James")
+                .password("1234")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
+                .roles("USER")
+                .build();
 
-        httpSecurity.authorizeHttpRequests(auth -> {
-            auth.anyRequest().authenticated();
-        });
+        UserDetails userAdmin = User.builder()
+                .username("Admin")
+                .password("12345")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
+                .roles("ADMIN")
+                .build();
 
-        httpSecurity.sessionManagement(
-                session -> {
-                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-                }
-        );
+        return new MapReactiveUserDetailsService(userAdmin, userJames);
+    }
 
-        httpSecurity.httpBasic();
-        httpSecurity.csrf().disable();
-        return httpSecurity.build();
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
 
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception
+    {
+        http.httpBasic();
+        http.formLogin();
+        http.csrf().disable();
+        http.authorizeExchange().pathMatchers(HttpMethod.GET, "/school/**").permitAll();
+        http.authorizeExchange().pathMatchers(HttpMethod.GET, "/h2-console/**").permitAll();
+        http.authorizeExchange().pathMatchers(HttpMethod.GET, "/student/**").authenticated();
+        return http.build();
     }
 }
